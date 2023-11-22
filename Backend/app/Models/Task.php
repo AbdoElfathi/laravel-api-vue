67b9c7dc68c5415750a711b1023c879f93cfa629
Abdo <abdessamadelfathi2018@gmail.com>
2023-11-22 15:09:39 +0100

    Api done

---
+++ Backend/app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'is_completed'];
+
+    // a taslk belong to a user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
