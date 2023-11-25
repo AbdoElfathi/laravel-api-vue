c8d0699b4893b2e9275db145cd0d70f9d8da7022
Abdo <abdessamadelfathi2018@gmail.com>
2023-11-25 14:25:37 +0100

    Project done

--- Frontend/vue-app/src/router/routes.js
+++ Frontend/vue-app/src/router/routes.js
@@ -1,12 +1,17 @@
 import TasksPage from "../pages/TasksPage.vue";
 import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
+import Summary from "../pages/SummaryPage.vue";
+import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue";
 
 const routes = [
   {
     path: "/tasks",
     component: TasksPage,
     name: "tasks",
+    meta: {
+      auth: true,
+    },
   },
   {
     path: "/login",
@@ -17,6 +22,19 @@ const routes = [
     path: "/register",
     component: RegisterPage,
     name: "register",
+  },
+  {
+    path: "/summary",
+    component: Summary,
+    name: "summary",
+    meta: {
+      auth: true,
+    },
+  },
+  {
+    path: "/:notFound(.*)",
+    component: NotFoundErrorPage,
+    name: "error.404",
   },
 ];
 
